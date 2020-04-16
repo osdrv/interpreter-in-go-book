3ddedfbfa6eabc7b7f94064a225d1bfff60ebde0
Oleg Sidorov <me@whitebox.io>
2020-04-16 22:57:04 +0200

    Chapter 4: array literals
    Signed-off-by: Oleg Sidorov <me@whitebox.io>

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -27,7 +27,8 @@ if (5 < 10) {
 10 == 10;
 10 != 9;
 "foobar"
-"foo bar"
+"foo bar";
+[1, 2];
 `
 
 	tests := []struct {
@@ -109,6 +110,13 @@ if (5 < 10) {
 		{token.SEMICOLON, ";"},
 		{token.STRING, "foobar"},
 		{token.STRING, "foo bar"},
+		{token.SEMICOLON, ";"},
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRACKET, "]"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
